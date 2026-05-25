9b78ede77c7af4374cfe278195a989914c7bb3e8
joaorez12 <joaorezende909@gmail.com>
2026-05-25 11:22:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll(){
+  for(let i = 0; i < reveals.length; i++){
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 120;
+
+    if(elementTop < windowHeight - elementVisible){
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
